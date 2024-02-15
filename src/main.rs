6b6af6aaee89e4c29015f0b24d799492ecda5c52
Elis Roberts <elisiforroberts@gmail.com>
2024-02-15 13:42:30 +0000

    feat: :sparkles: Add basic ratatui implementation of GUI
    Doing things slap-dash, but working example is a lot more interesting now.

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,105 @@
 #![allow(dead_code)]
-use nalgebra::{Isometry3, Translation3, UnitQuaternion, Vector3};
+use nalgebra::{Isometry3, Translation3, UnitDualQuaternion, UnitQuaternion, Vector3};
 use pdb_tui::{
     rasterizer::BasicAsciiRasterizer,
     render::{Canvas, Scene},
 };
-
 use std::time::Instant;
 
-fn main() {
-    // Playing around with lines
-    // let line = create_ray(10.0f32, 20.0f32);
-    // println!("{}", line)
+use crossterm::{
+    event::{self, KeyCode, KeyEventKind},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{
+    prelude::{CrosstermBackend, Stylize, Terminal},
+    text::Text,
+    widgets::Paragraph,
+};
+use std::io::{stdout, Result};
 
-    // TODO Load in a PDB file
+fn main() -> Result<()> {
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    terminal.clear()?;
 
-    // TODO Create a molecular surface for the PDB
-    // NOTE Should firstly look into how the rendering will work
-    // TODO Make each chain have its own colour
+    // TODO main loop
 
-    // TODO Set up scene and render
-
-    // TODO Look into Termion for a way to render PDB
-
-    // let meshes = get_meshes_from_obj(test_obj);
-    // let mesh = &meshes[0];
-    // let mut mesh = mesh.to_tri_mesh();
-
+    // Load and draw
     let test_obj = "./data/surface.obj";
     let mut scene = Scene::default();
     scene.load_meshes_from_path(test_obj);
-
-    let translation = Translation3::new(15.0f32, 15.0f32, -20.0f32);
-    let rotation = UnitQuaternion::from_scaled_axis(Vector3::y() * std::f32::consts::FRAC_PI_8);
-    let transform = Isometry3::from_parts(translation, rotation);
-    scene.transform_meshes(&transform);
-
+    // let translation = Translation3::new(5.0f32, 0.0f32, 0.0f32);
+    // let rotation = UnitQuaternion::from_scaled_axis(Vector3::y() * std::f32::consts::FRAC_PI_8);
+    // let rotation = UnitQuaternion::identity();
+    // let transform = Isometry3::from_parts(translation, rotation);
     let mut canvas = Canvas::<BasicAsciiRasterizer>::default();
 
-    let now = Instant::now();
-    println!("Starting to draw.");
     canvas.draw_scene_to_canvas(&scene);
-    let new_now = Instant::now();
-    println!("Drawn after {:?}", new_now.duration_since(now));
 
     // Print output to stdout
-    let frame_buffer = canvas.frame_buffer.clone();
-    let stdout: String = frame_buffer.iter().collect();
-    print!("{}", stdout);
+    // print!("{}", out_string);
 
-    let x_shift = Isometry3::from_parts(
-        Translation3::new(5.0f32, 0f32, 0f32),
-        UnitQuaternion::identity(),
-    );
-    for i in 0..10 {
-        scene.transform_meshes(&x_shift);
+    loop {
         canvas.draw_scene_to_canvas(&scene);
+        let out_string: String = canvas.frame_buffer.iter().collect();
+        terminal.draw(|frame| {
+            let area = frame.size();
+            frame.render_widget(Paragraph::new(Text::raw(&out_string)), area);
+        })?;
 
-        let path = format!("canvas_{}.png", i);
-        canvas.save_image(path).unwrap();
+        // Listen for keypress
+        if event::poll(std::time::Duration::from_millis(3))? {
+            if let event::Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press {
+                    match key.code {
+                        KeyCode::Char('q') => {
+                            break;
+                        }
+                        KeyCode::Char('l') => {
+                            let transform = Isometry3::translation(5f32, 0f32, 0f32);
+                            scene.transform_meshes(&transform);
+                        }
+                        KeyCode::Char('h') => {
+                            let transform = Isometry3::translation(-5f32, 0f32, 0f32);
+                            scene.transform_meshes(&transform);
+                        }
+                        KeyCode::Char('k') => {
+                            let transform = Isometry3::translation(0f32, 5f32, 0f32);
+                            scene.transform_meshes(&transform);
+                        }
+                        KeyCode::Char('j') => {
+                            let transform = Isometry3::translation(0f32, -5f32, 0f32);
+                            scene.transform_meshes(&transform);
+                        }
+                        KeyCode::Char('u') => {
+                            let transform = Isometry3::translation(0f32, 0f32, 5f32);
+                            scene.transform_meshes(&transform);
+                        }
+                        KeyCode::Char('d') => {
+                            let transform = Isometry3::translation(0f32, 0f32, -5f32);
+                            scene.transform_meshes(&transform);
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
     }
-}
 
-// TODO Add some tests for basic things
+    // let x_shift = Isometry3::from_parts(
+    //     Translation3::new(5.0f32, 0f32, 0f32),
+    //     UnitQuaternion::identity(),
+    // );
+    // for i in 0..10 {
+    //     scene.transform_meshes(&x_shift);
+    //     canvas.draw_scene_to_canvas(&scene);
+    //     let path = format!("canvas_{}.png", i);
+    //     canvas.save_image(path).unwrap();
+
+    // Cleanup
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
+}
