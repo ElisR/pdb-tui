aca07e0d8a81f2d2bb95e8af58f9b8b8ee91fe30
Elis Roberts <elisiforroberts@gmail.com>
2024-03-12 08:57:02 +0000

    refactor: :recycle: Move `run` function into separate module

--- src/gpu/pdb_gpu/main.rs
+++ src/gpu/pdb_gpu/main.rs
@@ -1,4 +1,4 @@
-use pdb_tui::gpu::pdb_gpu::run;
+use pdb_tui::gpu::pdb_gpu::run::run;
 
 fn main() {
     pollster::block_on(run());
--- src/gpu/pdb_gpu/mod.rs
+++ src/gpu/pdb_gpu/mod.rs
@@ -1,21 +1,17 @@
 //! Adapted Tutorial 10
 
 use std::iter;
-use tracing::warn;
 
 use cgmath::prelude::*;
 use wgpu::util::DeviceExt;
-use winit::{
-    event::*,
-    event_loop::{ControlFlow, EventLoop},
-    window::Window,
-};
+use winit::{event::*, window::Window};
 
-mod camera;
-mod instance;
-mod model;
-mod resources;
-mod texture;
+pub mod camera;
+pub mod instance;
+pub mod model;
+pub mod resources;
+pub mod run;
+pub mod texture;
 
 use camera::{Camera, CameraController, CameraUniform};
 use instance::{Instance, InstanceRaw, LightUniform};
@@ -488,66 +484,4 @@ impl State<WindowSpecificState> {
 
         Ok(())
     }
-}
-
-pub async fn run() {
-    env_logger::init();
-
-    let event_loop = EventLoop::new();
-    let title = env!("CARGO_PKG_NAME");
-    let window = winit::window::WindowBuilder::new()
-        .with_title(title)
-        .build(&event_loop)
-        .unwrap();
-
-    // State::new uses async code, so we're going to wait for it to finish
-    let mut state = State::new(window).await;
-
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Poll;
-        match event {
-            Event::MainEventsCleared => state.window().request_redraw(),
-            Event::WindowEvent {
-                ref event,
-                window_id,
-            } if window_id == state.window().id() => {
-                if !state.input(event) {
-                    match event {
-                        WindowEvent::CloseRequested
-                        | WindowEvent::KeyboardInput {
-                            input:
-                                KeyboardInput {
-                                    state: ElementState::Pressed,
-                                    virtual_keycode: Some(VirtualKeyCode::Escape),
-                                    ..
-                                },
-                            ..
-                        } => *control_flow = ControlFlow::Exit,
-                        WindowEvent::Resized(physical_size) => {
-                            state.resize(*physical_size);
-                        }
-                        WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                            state.resize(**new_inner_size);
-                        }
-                        _ => {}
-                    }
-                }
-            }
-            Event::RedrawRequested(window_id) if window_id == state.window().id() => {
-                state.update();
-                match state.render() {
-                    Ok(_) => {}
-                    // Reconfigure the surface if it's lost or outdated
-                    Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
-                        state.resize(state.inner_state.size)
-                    }
-                    // The system is out of memory, we should probably quit
-                    Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
-                    // We're ignoring timeouts
-                    Err(wgpu::SurfaceError::Timeout) => warn!("Surface timeout"),
-                }
-            }
-            _ => {}
-        }
-    });
 }
