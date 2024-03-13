a2da71f9e1adf049c3e639e823dd64ac5b6496e6
Elis Roberts <elisiforroberts@gmail.com>
2024-03-13 09:02:10 +0000

    feat: :sparkles: Create two separate entrypoints for windowed and windowless mode

--- src/gpu/pdb_gpu/main.rs
+++ src/gpu/pdb_gpu/main.rs
@@ -1,5 +1,6 @@
-use pdb_tui::gpu::pdb_gpu::run::run;
+#[allow(unused_imports)]
+use pdb_tui::gpu::pdb_gpu::run::run_windowed;
 
 fn main() {
-    pollster::block_on(run());
+    pollster::block_on(run_windowed());
 }
