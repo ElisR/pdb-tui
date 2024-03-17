b47d4e7f711a19b32aa2ea3762b0617a39ae6720
Elis Roberts <elisiforroberts@gmail.com>
2024-03-17 09:12:06 +0000

    refactor: :lipstick: Change the lighting so that background appears clear for TUI

--- src/basic_rasterizer.rs
+++ src/basic_rasterizer.rs
@@ -60,7 +60,7 @@ impl BasicAsciiRasterizer {
 impl Default for BasicAsciiRasterizer {
     fn default() -> Self {
         let gradient = vec!['@', '%', '#', '*', '+', '=', '-', ':', '.'];
-        let thresholds = vec![-0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
+        let thresholds = vec![-0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.999];
         let background = ' ';
         Self::new(gradient, thresholds, background).unwrap()
     }
